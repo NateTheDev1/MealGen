aabc297cb8f07f1649c565e767776b7662903b97
Nathaniel Richards <46858886+NateTheDev1@users.noreply.github.com>
2019-11-13 10:29:57 -0500

    [1.0.5] Added Favorites Page Functionality

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ let img = document.getElementById('meal-img');
 let category = document.getElementById('category');
 let ingredientList = document.getElementById('ingredients');
 let link = document.getElementById('meal-link');
+let favorite = document.getElementById('favorite');
+let favoriteList = document.getElementById('favorited-meals');
+let deleteMeal = document.getElementById('delete-meal');
 
+let fList = [];
+
+let curMeal;
+
+loadFavorites();
 // API EVENT
 
 let url = 'https://www.themealdb.com/api/json/v1/1/random.php';
@@ -19,6 +27,7 @@ gen.addEventListener('click', function(e) {
 		.then(function(data) {
 			let meal = data.meals[0];
 			updateMeal(meal);
+			curMeal = meal;
 		})
 		.catch(function(err) {
 			console.log(err);
@@ -31,4 +40,21 @@ function updateMeal(meal) {
 	category.textContent = meal.strCategory;
 	link.href = meal.strSource;
 	ingredientList.innerHTML = `<li class="list-group-item" id="ing-1">${meal.strIngredient1}</li><li class="list-group-item" id="ing-1">${meal.strIngredient2}</li><li class="list-group-item" id="ing-1">${meal.strIngredient3}</li>`;
+}
+
+// FAVORITE EVENT
+favorite.addEventListener('click', function(e) {
+	favoriteList.innerHTML += `<li class="list-group-item"><a href="${curMeal.strSource}">${curMeal.strMeal}</a> 
+  </li>`;
+	fList.push(`<li class="list-group-item"><a href="${curMeal.strSource}">${curMeal.strMeal}</a> 
+</li>`);
+});
+
+// DELETE EVENT
+
+function loadFavorites() {
+	favoriteList.innerHTML = '';
+	fList.forEach(function(meal) {
+		favoriteList.innerHTML += meal;
+	});
 }
